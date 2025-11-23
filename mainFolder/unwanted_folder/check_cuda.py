9de72630080af6e12bdbc5cf267a014a522091d9
Abhishek Jain <dracyrsaries@gmail.com>
2025-11-23 23:10:15 +0530

    Stop tracking generated data

--- mainFolder/unwanted_folder/check_cuda.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-print("CUDA available:", torch.cuda.is_available())
-if torch.cuda.is_available():
-    print("GPU Name:", torch.cuda.get_device_name(0))
-    print("Current Device:", torch.cuda.current_device())
